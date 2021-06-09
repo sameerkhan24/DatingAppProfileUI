dc9432f5164de33ab55e570fd50258e69f022dc0
Sameer Khan <sameerkhan241@outlook.com>
2021-06-10 03:44:57 +0530

    Added MyInfo widget with Radial Progress

--- lib/commons/myinfo.dart
+++ lib/commons/myinfo.dart
@@ -1,15 +1,52 @@
+import 'package:dating_app/commons/radialprogress.dart';
 import 'package:dating_app/commons/roundedimage.dart';
+import 'package:dating_app/styleguide/text_style.dart';
 import 'package:flutter/material.dart';
 
 class MyInfo extends StatelessWidget {
-  const MyInfo({ Key key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          RoundedImage(imagePath: "assets/images/anne.jpeg")
+          RadialProgress(
+            width: 4,
+            goalCompleted: 0.9,
+            child: RoundedImage(
+              imagePath: "assets/images/anne.jpeg",
+              size: Size.fromWidth(120.0),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Anne Grethe",
+                style: whiteNameTextStyle,
+              ),
+              Text(
+                ", 24",
+                style: whiteNameTextStyle,
+              ),
+            ],
+          ),
+          SizedBox(height: 10,),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  "assets/icons/location_pin.png",
+                  width: 20.0,
+                  color: Colors.white,
+                ),
+                Text(
+                  "  34 kilometers",
+                  style: whiteSubHeadingTextStyle,
+                )
+              ],
+          ),
         ],
       ),
     );
